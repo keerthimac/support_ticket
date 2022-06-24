6d73ac08f2b115e1d6cf5dc0503b3eaa11364c02
Darshana <keerthi.mac@gmail.com>
2022-06-24 07:57:47 +0530

    userCon &userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("server....");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to support desk API" });
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
