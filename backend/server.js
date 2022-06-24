749fe9ad7ec32d579695a69dde742e3292ec9d81
Darshana <keerthi.mac@gmail.com>
2022-06-25 00:19:14 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8000;
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to support desk API" });
@@ -10,6 +19,8 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
