463c31a13b44a9e098b4b931bd7e7378501d1f21
Darshana <keerthi.mac@gmail.com>
2022-06-25 07:28:46 +0530

    database connceted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModels");
 // @desc Register a new user
 // @route /api/users
 // @access Public
@@ -11,7 +12,31 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please include all fields");
   }
-  res.send("Register Route");
+
+  // Find a user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
 });
 
 // @desc Register a new user
