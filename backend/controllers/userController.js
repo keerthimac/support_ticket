9d67adc23fccd6617c0c1e23eefe166c31e95701
Darshana <keerthi.mac@gmail.com>
2022-06-26 11:56:19 +0530

    auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Register a new user
+// @desc Login user
 // @route /api/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
@@ -69,7 +69,20 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get Current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+const User = require("../models/userModels");
+
+const protect = asyncHandler(async (req, res, next) => {
+  //01 Initialize variable for token
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //03 Get token from header
+      token = req.headers.authorization.split(" ")[1];
+      //04 Verify Token
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      //05 Get user from token via server
+      req.user = await User.findById(verified.user_id).select("-password");
+      //06 next function
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
+
+module.exports = { protect };
