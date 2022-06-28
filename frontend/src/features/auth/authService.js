154b46cbaee2b50d8f4324cae863ba60cdec4464
Darshana <keerthi.mac@gmail.com>
2022-06-28 07:18:22 +0530

    login & logout user

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "/api/users";
+const API_URL_LOGIN = "/api/users/login";
 
 //Register User
 const register = async (userData) => {
@@ -12,8 +13,23 @@ const register = async (userData) => {
   return response.data;
 };
 
+//Login User
+const login = async (userData) => {
+  const response = await axios.post(API_URL_LOGIN, userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+//Logout User
+
+const logout = () => localStorage.removeItem("user");
+
 const authService = {
   register,
+  login,
+  logout,
 };
 
 export default authService;
