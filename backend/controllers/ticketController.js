21d96dd428d9169a4f64cbab247c8bf0707e2afc
Darshana <keerthi.mac@gmail.com>
2022-06-29 07:51:52 +0530

    get ticket function

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -45,11 +45,38 @@ const createTicket = asyncHandler(async (req, res) => {
     user: req.user.id,
     status: "new",
   });
-
   res.status(201).json({ ticket });
 });
 
+// @desc Get user Ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  //get user using the  ID in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 module.exports = {
+  getTicket,
   getTickets,
   createTicket,
 };
